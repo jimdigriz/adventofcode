4890d8d838b5f728b38a23b4423ff17d0e134861
Alexander Clouter <alex@digriz.org.uk>
2021-12-01 22:26:19 +0000

    part2

--- 2021/01/aoc.erl
+++ 2021/01/aoc.erl
@@ -2,24 +2,48 @@
 
 -on_load(init/0).
 
--export([run/0, run/1]).
+-export([part1/0, part1/1]).
+-export([part2/0, part2/1]).
 
 init() ->
 	lists:foreach(fun({K,F}) ->
 		{ok,B} = file:read_file(atom_to_list(?MODULE) ++ "." ++ F),
 		L = lists:map(fun binary_to_integer/1, binary:split(B, <<"\n">>, [global,trim_all])),
 		persistent_term:put({?MODULE,K}, L)
-	end, [{input,"input"},{example,"input.example"}]).
+	end, [{example,"input.example"},{input,"input"}]).
 
-run() ->
-	run(input).
+part1() ->
+	part1(example).
 
-run(X) when X == example; X == input ->
-	run(persistent_term:get({?MODULE,X}));
-run([I|R]) ->
+part1(example) ->
+	7 = run(persistent_term:get({?MODULE,example}));
+part1(input) ->
+	run(persistent_term:get({?MODULE,input})).
+
+part2() ->
+	part2(example).
+
+part2(example) ->
+	5 = run(persistent_term:get({?MODULE,example}), 3);
+part2(input) ->
+	run(persistent_term:get({?MODULE,input}), 3).
+
+run(L) ->
+	run(L, 1).
+
+run(L, W) ->
+	run(L, W, []).
+
+run(L = [_X|R], W, A) when length(L) >= W ->
+	AN = lists:sum(element(1, lists:split(W, L))),
+	run(R, W, [AN|A]);
+run(_L, _W, A) ->
+	run2(lists:reverse(A)).
+
+run2([I|R]) ->
 	element(1, lists:foldl(fun
-		(XX, {A,L}) when XX > L ->
+		(XX,{A,L}) when XX > L ->
 			{A+1,XX};
-		(XX, {A,_L}) ->
+		(XX,{A,_L}) ->
 			{A,XX}
 	end, {0,I}, R)).
