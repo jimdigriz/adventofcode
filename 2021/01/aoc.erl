d2ebc40b80d0a26559a1e1f212488f06dbf12cf1
Alexander Clouter <alex@digriz.org.uk>
2021-12-02 08:02:18 +0000

    ~5x faster (time and number of reductions)

--- 2021/01/aoc.erl
+++ 2021/01/aoc.erl
@@ -31,18 +31,20 @@ part2(input) ->
 run(V) ->
 	run(V, 1).
 run(V, W) ->
-	run(persistent_term:get({?MODULE,V}), W, []).
-
-run(L = [_X|R], W, A) when length(L) >= W ->
-	AN = lists:sum(element(1, lists:split(W, L))),
-	run(R, W, [AN|A]);
-run(_L, _W, A) ->
-	run2(lists:reverse(A)).
+	L = persistent_term:get({?MODULE,V}),
+	{H,T} = lists:split(W, L),
+	run(H, T, []).
+run(H = [_HH|HR], _T = [TH|TR], A) ->
+	S = lists:sum(H),
+	run(HR ++ [TH], TR, [S|A]);
+run(H, _T = [], A) ->
+	S = lists:sum(H),
+	run2(lists:reverse([S|A])).
 
 run2([I|R]) ->
-	element(1, lists:foldl(fun
-		(XX,{A,L}) when XX > L ->
-			{A+1,XX};
-		(XX,{A,_L}) ->
-			{A,XX}
-	end, {0,I}, R)).
+	element(2, lists:foldl(fun
+		(X,{L,A}) when X > L ->
+			{X,A+1};
+		(X,{_L,A}) ->
+			{X,A}
+	end, {I,0}, R)).
