0c55e892126700148c5be8c09fe983cea8dce86a
Alexander Clouter <alex@digriz.org.uk>
2021-12-07 15:23:09 +0000

    day 7 part 2

--- 2021/07/aoc.erl
+++ 2021/07/aoc.erl
@@ -5,9 +5,9 @@
 -on_load(init/0).
 
 -export([part1/0, part1/1]).
-%-export([part2/0, part2/1]).
+-export([part2/0, part2/1]).
 
--compile(export_all).
+%-compile(export_all).
 
 init() ->
 	lists:foreach(fun({K,F}) ->
@@ -23,27 +23,39 @@ to_list_of_ints(B) ->
 part1() ->
 	part1(example).
 part1(example) ->
-	{2,37} = run1(example);
+	{2,37} = run(example, fun fuel1/2);
 part1(input) ->
-	run1(input).
+	run(input, fun fuel1/2).
 
-run1(V) ->
+part2() ->
+	part2(example).
+part2(example) ->
+	{5,168} = run(example, fun fuel2/2);
+part2(input) ->
+	run(input, fun fuel2/2).
+
+run(V, FuelFun) ->
 	L = persistent_term:get({?MODULE,V}),
 	{Min,Max} = lists:foldl(fun(X, {LL,HH}) ->
 		{min(X,LL),max(X,HH)}
 	end, {0,0}, L),
-	run1(L, Min, Max, undefined, infinity, Min).
+	run(L, FuelFun, Min, Max, undefined, infinity, Min).
 
-run1(_L, _Min, Max, BestPosition, BestFuel, Position) when Position > Max ->
+run(_L, _FuelFun, _Min, Max, BestPosition, BestFuel, Position) when Position > Max ->
 	{BestPosition,BestFuel};
-run1(L, Min, Max, BestPosition0, BestFuel0, Position) ->
-	{BestPosition, BestFuel} = case fuel(L, Position) of
+run(L, FuelFun, Min, Max, BestPosition0, BestFuel0, Position) ->
+	{BestPosition, BestFuel} = case FuelFun(L, Position) of
 		Fuel when Fuel < BestFuel0 ->
 			{Position,Fuel};
 		_Fuel ->
 			{BestPosition0,BestFuel0}
 	end,
-	run1(L, Min, Max, BestPosition, BestFuel, Position + 1).
+	run(L, FuelFun, Min, Max, BestPosition, BestFuel, Position + 1).
 
-fuel(L, Position) ->
+fuel1(L, Position) ->
 	lists:foldl(fun(X, A) -> A + abs(X - Position) end, 0, L).
+
+fuel2(L, Position) ->
+	lists:foldl(fun(X, A) ->
+		A + lists:sum(lists:seq(1, abs(X - Position)))
+	end, 0, L).
