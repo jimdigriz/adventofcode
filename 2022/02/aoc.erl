22bddd45bbdd30024cea51fab6cce22b9c4057f6
Alexander Clouter <alex@digriz.org.uk>
2022-12-04 18:59:38 +0000

    day 2 part 2

--- 2022/02/aoc.erl
+++ 2022/02/aoc.erl
@@ -5,6 +5,7 @@
 -on_load(init/0).
 
 -export([part1/0, part1/1]).
+-export([part2/0, part2/1]).
 
 %-compile(export_all).
 
@@ -16,25 +17,16 @@ init() ->
 	lists:foreach(fun({K,F}) ->
 		{ok,B} = file:read_file(atom_to_list(?MODULE) ++ "." ++ F),
 		L0 = binary:split(B, <<"\n">>, [global,trim]),
-		L = lists:map(fun(<<X, " ", Y>>) -> {map(X),map(Y)} end, L0),
+		L = [ {map(X),map(Y)} || <<X, " ", Y>> <- L0 ],
 		persistent_term:put({?MODULE,K}, L)
 	end, [{example,"input.example"},{input,"input"}]).
 
-map(X) when X == $A; X == $X -> rock;
-map(X) when X == $B; X == $Y -> paper;
-map(X) when X == $C; X == $Z -> scissors.
+map(X) when X >= $A, X =< $C -> X - $A;
+map(X) when X >= $X, X =< $Z -> X - $X.
 
-score(X, Y) when X == Y -> item(Y) + ?DRAW;
-score(rock, Y = paper) -> item(Y) + ?WIN;
-score(rock, Y = scissors) -> item(Y) + ?LOSE;
-score(paper, Y = rock) -> item(Y) + ?LOSE;
-score(paper, Y = scissors) -> item(Y) + ?WIN;
-score(scissors, Y = rock) -> item(Y) + ?WIN;
-score(scissors, Y = paper) -> item(Y) + ?LOSE.
-
-item(rock) -> 1;
-item(paper) -> 2;
-item(scissors) -> 3.
+score(X, Y) when X == Y -> (Y + 1) + ?DRAW;
+score(X, Y) when X == (3 + Y - 1) rem 3 -> (Y + 1) + ?WIN;
+score(X, Y) when X == (Y + 1) rem 3 -> (Y + 1) + ?LOSE.
 
 %%%
 
@@ -47,9 +39,21 @@ part1(input) ->
 
 run1(V) ->
 	L = persistent_term:get({?MODULE,V}),
-	do_run1(L).
-
-do_run1(L) ->
-	lists:foldl(fun({X,Y}, A) -> score(X, Y) + A end, 0, L).
+	lists:sum([ score(X, Y) || {X,Y} <- L ]).
 
 %%%
+
+part2() ->
+	part2(example).
+part2(example) ->
+	12 = run2(example);
+part2(input) ->
+	run2(input).
+
+run2(V) ->
+	L = persistent_term:get({?MODULE,V}),
+	lists:sum([ score(X, play(X, Y)) || {X,Y} <- L ]).
+
+play(X, _Y = 0) -> (3 + X - 1) rem 3;
+play(X, _Y = 1) -> X;
+play(X, _Y = 2) -> (X + 1) rem 3.
