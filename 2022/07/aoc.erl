bfd869f18573a950f2bd6f3a75e3ff03af69842b
Alexander Clouter <alex@digriz.org.uk>
2022-12-07 08:11:37 +0000

    day 7 part 2

--- 2022/07/aoc.erl
+++ 2022/07/aoc.erl
@@ -5,9 +5,12 @@
 -on_load(init/0).
 
 -export([part1/0, part1/1]).
-%-export([part2/0, part2/1]).
+-export([part2/0, part2/1]).
 
 %-compile(export_all).
+
+-define(TOTAL, 70000000).
+-define(NEED, 30000000).
 
 init() ->
 	lists:foreach(fun({K,F}) ->
@@ -36,6 +39,25 @@ parse([C = <<X, _/binary>>|R], CWD, FS0) when X >= $0, X =< $9 ->
 parse([], _CWD, FS) ->
 	FS.
 
+get_dirs({_K = [{file,_FF}|DD],SS,I}, FS0) ->
+	FS = get_dirs2(DD, SS, FS0),
+	get_dirs(gb_trees:next(I), FS);
+get_dirs({_K,_V,I}, D) ->
+	get_dirs(gb_trees:next(I), D);
+get_dirs(none, D) ->
+	D.
+
+get_dirs2([], _S, FS) ->
+	FS;
+get_dirs2(D, S, FS0) ->
+	{SS,FS} = case lists:keytake(D, 1, FS0) of
+		{value,{D,SSS},FS1} ->
+			{S + SSS,FS1};
+		false ->
+			{S,FS0}
+	end,
+	get_dirs2(tl(D), S, [{D,SS}|FS]).
+
 %%%
 
 part1() ->
@@ -56,21 +78,20 @@ run1(V) ->
 	end, D0),
 	lists:sum(D).
 
-get_dirs({_K = [{file,_FF}|DD],SS,I}, FS0) ->
-	FS = get_dirs2(DD, SS, FS0),
-	get_dirs(gb_trees:next(I), FS);
-get_dirs({_K,_V,I}, D) ->
-	get_dirs(gb_trees:next(I), D);
-get_dirs(none, D) ->
-	D.
+%%%
 
-get_dirs2([], _S, FS) ->
-	FS;
-get_dirs2(D, S, FS0) ->
-	{SS,FS} = case lists:keytake(D, 1, FS0) of
-		{value,{D,SSS},FS1} ->
-			{S + SSS,FS1};
-		false ->
-			{S,FS0}
-	end,
-	get_dirs2(tl(D), S, [{D,SS}|FS]).
+part2() ->
+	part2(example).
+part2(example) ->
+	24933642 = run2(example);
+part2(input) ->
+	run2(input).
+
+run2(V) ->
+	T = persistent_term:get({?MODULE,V}),
+	D = get_dirs(gb_trees:next(gb_trees:iterator(T)), []),
+	Free = ?TOTAL - element(2, lists:keyfind([<<"/">>], 1, D)),
+	[{_DD,SS}|_] = lists:dropwhile(fun({_DDD,SSS}) ->
+		(Free + SSS) < ?NEED
+	end, lists:keysort(2, D)),
+	SS.
