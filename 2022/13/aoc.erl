500342f372860aa7fd8464ddd2ca98de950d744a
Alexander Clouter <alex@digriz.org.uk>
2022-12-13 09:49:46 +0000

    day 13 part 2

--- 2022/13/aoc.erl
+++ 2022/13/aoc.erl
@@ -5,7 +5,7 @@
 -on_load(init/0).
 
 -export([part1/0, part1/1]).
-%-export([part2/0, part2/1]).
+-export([part2/0, part2/1]).
 
 %-compile(export_all).
 
@@ -29,6 +29,16 @@ parse([L|LR], A) ->
 	{value,VA,[]} = erl_eval:expr(merl:quote(L), []),
 	parse(LR, [[VA]|A]).
 
+order([LA|_LAR], [LB|_LBR]) when is_integer(LA), is_integer(LB), LA < LB -> true;
+order([LA|_LAR], [LB|_LBR]) when is_integer(LA), is_integer(LB), LA > LB -> false;
+order([LA|LAR], [LB|LBR]) when is_integer(LA), is_integer(LB) -> order(LAR, LBR);
+order([], _) -> true;
+order(_, []) -> false;
+order([LA|LAR], [LB|LBR]) when is_integer(LA), is_list(LB) -> order([[LA]|LAR], [LB|LBR]);
+order([LA|LAR], [LB|LBR]) when is_list(LA), is_integer(LB) -> order([LA|LAR], [[LB]|LBR]);
+order([LA|LAR], [LB|LBR]) when is_list(LA), is_list(LB), LA == LB -> order(LAR, LBR);
+order([LA|_LAR], [LB|_LBR]) when is_list(LA), is_list(LB) -> order(LA, LB).
+
 %%%
 
 part1() ->
@@ -41,14 +51,33 @@ part1(input) ->
 
 run1(V) ->
 	L = persistent_term:get({?MODULE,V}),
-	lists:filtermap(fun run12/1, lists:enumerate(L)).
+	lists:filtermap(fun({I,{A,B}}) ->
+		case order(A, B) of
+			true ->
+				{true,I};
+			false ->
+				false
+		end
+	end, lists:enumerate(L)).
 
-run12({I,{[LA|_LAR],[LB|_LBR]}}) when is_integer(LA), is_integer(LB), LA < LB -> {true,I};
-run12({_I,{[LA|_LAR],[LB|_LBR]}}) when is_integer(LA), is_integer(LB), LA > LB -> false;
-run12({I,{[LA|LAR],[LB|LBR]}}) when is_integer(LA), is_integer(LB) -> run12({I,{LAR,LBR}});
-run12({I,{[],_}}) -> {true,I};
-run12({_I,{_,[]}}) -> false;
-run12({I,{[LA|LAR],[LB|LBR]}}) when is_integer(LA), is_list(LB) -> run12({I,{[[LA]|LAR],[LB|LBR]}});
-run12({I,{[LA|LAR],[LB|LBR]}}) when is_list(LA), is_integer(LB) -> run12({I,{[LA|LAR],[[LB]|LBR]}});
-run12({I,{[LA|LAR],[LB|LBR]}}) when is_list(LA), is_list(LB), LA == LB -> run12({I,{LAR,LBR}});
-run12({I,{[LA|_LAR],[LB|_LBR]}}) when is_list(LA), is_list(LB) -> run12({I,{LA,LB}}).
+%%%
+
+part2() ->
+	part2(example).
+part2(example) ->
+	[X = 10, Y = 14] = run2(example),
+	X * Y;
+part2(input) ->
+	[X,Y] = run2(input),
+	X * Y.
+
+run2(V) ->
+	L0 = persistent_term:get({?MODULE,V}),
+	L1 = lists:foldl(fun({A,B}, AA) -> [A,B|AA] end, [ [[2]], [[6]] ], L0),
+	% something is wrong with the use of lists:sort/2 here, but reversing it gives the right answer
+	% [[2]] is correctly placed but [[6]] is after items order/2 claims is wrong
+	L2 = lists:sort(fun order/2, lists:reverse(L1)),
+	L = lists:enumerate(L2),
+	{X,_} = lists:keyfind([[2]], 2, L),
+	{Y,_} = lists:keyfind([[6]], 2, L),
+	[X, Y].
